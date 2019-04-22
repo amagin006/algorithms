ca200f8e09914168bbc338c743e3eb6b307d674f
Shota Iwamoto <amaginqoo@gmail.com>
2019-04-22 11:44:56 -0700

    Initial Commit

---
+++ Leetcode/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Leetcode
+//
+//  Created by Shota Iwamoto on 2019-04-13.
+//  Copyright © 2019 Shota Iwamoto. All rights reserved.
+//
+
+import Foundation
+
+var s = [ 5, 1, 9, 11, 7, 8, 13, 2, 3]
+
+//var b = [9]
+//print(reverseElement(&s))
+
+heapSort(&s)
