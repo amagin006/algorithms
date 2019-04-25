6d0d5bdff6b6c550237354bc8d501eea2e52377d
Shota Iwamoto <amaginqoo@gmail.com>
2019-04-25 00:46:03 -0700

    Add gitignore and 242

--- Leetcode/main.swift
+++ Leetcode/main.swift
@@ -8,9 +8,6 @@
 
 import Foundation
 
-var s = [ 5, 1, 9, 11, 7, 8, 13, 2, 3]
-
-//var b = [9]
-//print(reverseElement(&s))
-let c = "cc"
-print(firstUniqChar(c))
+let s = "aacc"
+let t = "ccac"
+print(isAnagram(s, t))
