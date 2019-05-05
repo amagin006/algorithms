a5769204b71cd00f9b91823c12305c6434034fd2
Shota Iwamoto <amaginqoo@gmail.com>
2019-05-05 11:34:17 -0700

    Add 088

--- Leetcode/main.swift
+++ Leetcode/main.swift
@@ -8,5 +8,10 @@
 
 import Foundation
 
-let s = ["dog","racecar","car"]
-print(longestCommonPrefix(s))
+
+var nums1 = [1, 2, 3, 0, 0, 0]
+let m = 3
+let nums2 = [2, 5, 6]
+let n = 3
+
+merge(&nums1, m, nums2, n)
